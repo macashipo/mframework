2ce491ecd0446a6fe95f1e36d3d7defe56e8ccbf
macashipo <macashipo@yahoo.com>
2016-05-13 10:26:46 +0700

    Update NSBundle.h

--- MLibrary/MCore/Additions/Foundation/NSBundle.h
+++ MLibrary/MCore/Additions/Foundation/NSBundle.h
@@ -7,6 +7,18 @@
  // \____|__  /\______  /|____|
  //         \/        \/
  //
- // 29/03/2015
+ // 14/04/2015
  //================================
+ // Need import MFBundle.bundle to project
+ //================================
+ // 
  */
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NSBundle (M)
++ (NSBundle *)  m_bundle;
++ (UIImage *)   m_imagePNGFromMBundle:(NSString *)name;
++ (UIImage *)   m_imageFromMBundle:(NSString *)name withExt:(NSString *)ext;
+@end
