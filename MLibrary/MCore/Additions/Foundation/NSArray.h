d8bf9ec09b42387c1fc0df145ca30cd061498b2d
macashipo <macashipo@yahoo.com>
2016-05-13 10:20:44 +0700

    Update NSArray.h

--- MLibrary/MCore/Additions/Foundation/NSArray.h
+++ MLibrary/MCore/Additions/Foundation/NSArray.h
@@ -10,3 +10,20 @@
  // 29/03/2015
  //================================
  */
+#import <Foundation/Foundation.h>
+
+@interface NSArray (M)
+- (NSArray *)   m_reversedArray;
+- (NSString *)  m_jsonString;
+- (id)          m_objectAtIndexSafe:(NSUInteger)index;
+- (id)          m_objectAtIndexCircle:(NSInteger)index;
+- (NSArray *)   m_shuffledArray;
+- (NSArray *)   m_uniqueObjects;
+
++ (NSString *)  m_jsonString:(NSArray *)array;
++ (NSArray *)   m_reversedArray:(NSArray *)array;
+@end
+
+@interface NSMutableArray (M)
+- (void)        m_shuffle;
+@end
