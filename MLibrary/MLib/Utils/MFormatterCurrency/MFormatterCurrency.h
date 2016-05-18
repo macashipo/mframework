a11902c71fd559f732e861103ff582be1c45f0d0
macashipo <macashipo@yahoo.com>
2016-05-18 10:48:48 +0700

    Update MFormatterCurrency.h

--- MLibrary/MLib/Utils/MFormatterCurrency/MFormatterCurrency.h
+++ MLibrary/MLib/Utils/MFormatterCurrency/MFormatterCurrency.h
@@ -10,3 +10,20 @@
  // 17/05/2016
  //================================
  */
+
+#import <Foundation/Foundation.h>
+
+@interface MFormatterCurrency : NSObject
+
+@end
+
+@interface NSNumberFormatter (MCurrency)
++ (id)m_formatterWithCurrency:(NSString *)locale;
++ (id)m_formatterWithCurrencyVND;
+@end
+
+@interface NSString (MCurrecy)
+
++(NSString*)m_currencyVNDWith:(NSNumber *)number;
++(NSString*)m_currencyVNDWithInt:(NSInteger)number;
+@end
