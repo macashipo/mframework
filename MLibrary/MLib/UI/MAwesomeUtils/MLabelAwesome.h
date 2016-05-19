f16fed71860808a06c8b17a9fbfeb1acf70cd888
macashipo <macashipo@yahoo.com>
2016-05-19 15:36:08 +0700

    Create MLabelAwesome.h

---
+++ MLibrary/MLib/UI/MAwesomeUtils/MLabelAwesome.h
@@ -0,0 +1,20 @@
+/*
+ //================================
+ //    _____  ____________________
+ //   /     \ \_   ___ \__    ___/
+ //  /  \ /  \/    \  \/ |    |
+ // /    Y    \     \____|    |
+ // \____|__  /\______  /|____|
+ //         \/        \/
+ //
+ // 19/05/2016
+ //================================
+ */
+
+#import <UIKit/UIKit.h>
+#import "MLabelIcon.h"
+@interface MLabelAwesome : MLabelIcon
+@property (nonatomic) IBInspectable UIColor *iconColor;
+@property (nonatomic) IBInspectable NSString *iconIconIdentifier;
+@property (nonatomic) IBInspectable NSInteger marginIconAwesome;
+@end
