6ecd714b1f3a29b844c64c34d478ad66a9bcb3b5
macashipo <macashipo@yahoo.com>
2016-05-19 15:36:46 +0700

    Create MLabelIcon.h

---
+++ MLibrary/MLib/UI/MCustomView/MLabelIcon.h
@@ -0,0 +1,21 @@
+/*
+ //================================
+ //    _____  ____________________
+ //   /     \ \_   ___ \__    ___/
+ //  /  \ /  \/    \  \/ |    |
+ // /    Y    \     \____|    |
+ // \____|__  /\______  /|____|
+ //         \/        \/
+ //
+ // 19/05/2016
+ //================================
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface MLabelIcon : UILabel
+@property (nonatomic) IBInspectable UIImage *imgLeftIcon;
+@property (nonatomic) IBInspectable NSInteger sizeSquareIcon;
+@property (nonatomic) IBInspectable NSInteger marginIcon;
+@property (nonatomic) IBInspectable BOOL isIconRight;
+@end
